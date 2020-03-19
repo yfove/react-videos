4c2cb54fb903bd83583ae18bf6092e273a971c5b
cfue <curtis.ch.chat@gmail.com>
2020-03-19 17:17:30 -0600

    passing state as props

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
+import VideoList from "./VideoList";
 
 const KEY = "AIzaSyCywnPZhhVweBHzCQmt1dZiOIAxlzcrY7M";
 
@@ -21,6 +22,7 @@ class App extends React.Component {
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />I have{" "}
         {this.state.videos.length} videos
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
