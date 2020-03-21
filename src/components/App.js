924c95bb45ece1e4cab3027ed729cb1924489675
cfue <curtis.ch.chat@gmail.com>
2020-03-21 13:41:40 -0600

    conditional rendering

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,10 @@ import React from "react";
 import SearchBar from "./SearchBar";
 import youtube from "../apis/youtube";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
 
   onTermSubmit = async term => {
     const response = await youtube.get("/search", {
@@ -20,14 +21,19 @@ class App extends React.Component {
   };
 
   onVideoSelect = video => {
-    console.log("From the App!", video);
+    this.setState({ selectedVideo: video });
+    // console.log("From the App!", video);
   };
 
   render() {
     return (
       <div className="ui container">
         <SearchBar onFormSubmit={this.onTermSubmit} />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={this.onVideoSelect}
+          videos={this.state.videos}
+        />
       </div>
     );
   }
