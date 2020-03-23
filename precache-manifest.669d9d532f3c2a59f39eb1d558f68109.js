96222f722b61e0dda6aa3d9d53265c3722b48883
cfue <curtis.ch.chat@gmail.com>
2020-03-23 17:04:16 -0600

    Updates

---
+++ precache-manifest.669d9d532f3c2a59f39eb1d558f68109.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6f350547b21035051bd961d71cd4fbc",
+    "url": "/react-videos/index.html"
+  },
+  {
+    "revision": "45b3967f75728c8c4f7c",
+    "url": "/react-videos/static/css/main.4a966e5b.chunk.css"
+  },
+  {
+    "revision": "c97893a48f7393dc346f",
+    "url": "/react-videos/static/js/2.86213445.chunk.js"
+  },
+  {
+    "revision": "59fc29711a22a223e9f602decc11b79f",
+    "url": "/react-videos/static/js/2.86213445.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "45b3967f75728c8c4f7c",
+    "url": "/react-videos/static/js/main.9e7737c2.chunk.js"
+  },
+  {
+    "revision": "b0f0eabbfeaaf6fb23b0",
+    "url": "/react-videos/static/js/runtime-main.136cf512.js"
+  }
+]);
